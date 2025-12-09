1b4a598a3ed437d8e1e85ba7c7d3e632cd695ab3
Sreenath M P <sreenath.MP@cognizant.com>
2025-12-09 19:03:38 +0530

    Update video_player.js

--- video_player.js
+++ video_player.js
@@ -42,6 +42,7 @@
       const onInnerDomReady = async () => {
         try {
           // Wait for target elements
+          console.log("body content", innerDox.getElementById("mediaContent");
           const mediaContainer = await waitForEl(innerDoc, '#rscpAu-MediaContainer');
           const video = await waitForEl(innerDoc, '#rscpAu-Media');
 
